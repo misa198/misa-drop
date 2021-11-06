883bd8888117a4d3186e7733cae888a50448f20b
Thành Vũ <csnsjh@gmail.com>
2021-11-06 22:15:50 +0700

    feat: switch locale

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,30 +1,42 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import { FC } from 'react';
 import { Info, Moon, Sun } from 'react-feather';
 import Tooltip from 'react-tooltip';
 import { useAppDispatch, useAppSelector } from '../app/hooks/redux';
+import { useTranslate } from '../app/hooks/translation';
 import { themeActions } from '../app/store/slices/theme.slice';
 import logoLight from '../public/logo-light.svg';
 import logo from '../public/logo.svg';
 
 const Header: FC = () => {
+  const { t, locale } = useTranslate();
+  const router = useRouter();
+  const { pathname, query, asPath } = router;
   const dispatch = useAppDispatch();
   const darkMode = useAppSelector((state) => state.theme.darkMode);
+  const otherLocale = locale === 'en' ? 'vi' : 'en';
 
   function onChangeTheme() {
     dispatch(themeActions.setTheme({ darkMode: !darkMode }));
     localStorage.setItem('darkMode', JSON.stringify(!darkMode));
   }
 
+  function onChangeLocale() {
+    router.push({ pathname, query }, asPath, { locale: otherLocale });
+  }
+
   const navLinks = [
     {
       Icon: darkMode ? Sun : Moon,
-      tooltip: 'Change mode',
+      tooltip: darkMode
+        ? t('app.header.light-mode')
+        : t('app.header.dark-mode'),
       onClick: onChangeTheme,
     },
     {
       Icon: Info,
-      tooltip: 'About us',
+      tooltip: t('app.header.about'),
     },
   ];
 
@@ -38,6 +50,17 @@ const Header: FC = () => {
         className="pointer-events-none"
       />
       <div className="flex">
+        <button
+          className="uppercase text-xl outline-none ml-5 cursor-pointer"
+          data-for={`nav-link-${otherLocale}`}
+          data-tip
+          onClick={onChangeLocale}
+        >
+          {otherLocale}
+          <Tooltip id={`nav-link-${otherLocale}`} type="info" effect="solid">
+            <span>{t(`app.header.${otherLocale}`)}</span>
+          </Tooltip>
+        </button>
         {navLinks.map(({ Icon, tooltip, onClick }, index) => (
           <button
             key={index}
