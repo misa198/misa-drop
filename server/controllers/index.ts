d070e25a5bad39824bdeb13f36555bfa3ab0762e
Thành Vũ <csnsjh@gmail.com>
2021-11-07 15:40:09 +0700

    chore: change start cmd

--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -3,6 +3,12 @@ import { signToken } from '../services/jwt';
 
 export const getToken = (req: Request, res: Response) => {
   const ip = req.clientIp as string;
+  const pip =
+    (req.headers && req.headers['x-forwarded-for']) ||
+    req.ip ||
+    (req.connection && req.connection.remoteAddress);
+  console.log(pip);
+  console.log(ip);
   const token = signToken(ip);
   return res.json({ token });
 };
