918b1d920f4e112c688aac35701e6d5880e0bd60
Thành Vũ <csnsjh@gmail.com>
2021-11-08 18:42:50 +0700

    fix: fix css

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,13 @@
 import { FC } from 'react';
 import { useAppSelector } from '../app/hooks/redux';
 import User from './User';
+import styles from './Footer.module.css';
 
 const Footer: FC = () => {
   const user = useAppSelector((state) => state.user);
 
   return (
-    <div className="w-full flex justify-center" style={{ height: '150px' }}>
+    <div className={`w-full flex justify-center ${styles.footer}`}>
       <User name={user.name} color={user.color} />
     </div>
   );
